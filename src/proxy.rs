bc5f9bfbf3f3f4106ebdac832a7d8cf704336c10
Cherry <13651622+MolotovCherry@users.noreply.github.com>
2026-07-31 07:12:33 -0700

    Make proxy method slightly more robust and easy to read

--- src/proxy.rs
+++ src/proxy.rs
@@ -3,43 +3,53 @@ use std::{convert::Infallible, sync::Arc};
 use axum::{
     body::Body,
     extract::{Request, State},
-    http::StatusCode,
+    http::{HeaderMap, HeaderValue, Method, StatusCode, Uri},
     response::Response,
 };
-use tracing::info;
+use tracing::{error, info};
 
 use crate::{StateData, error_pages::error_page, utils::format_req};
 
 pub async fn proxy(
     State(state): State<Arc<StateData>>,
-    req: Request<Body>,
+    uri: Uri,
+    method: Method,
+    headers: HeaderMap<HeaderValue>,
+    body: Body,
 ) -> Result<Response<Body>, Infallible> {
-    let (parts, body) = req.into_parts();
+    let path = uri.path_and_query().map(|i| i.as_str()).unwrap_or("/");
 
-    let path = parts
-        .uri
-        .path_and_query()
-        .map(|i| i.as_str())
-        .unwrap_or("/");
-
-    let uri = format!("http://{}{path}", state.config.addresses.backend);
-    let mut builder = Request::builder().method(&parts.method).uri(uri);
-    if let Some(hm) = builder.headers_mut() {
-        *hm = parts.headers;
+    let url = format!("http://{}{path}", state.config.addresses.backend);
+    let mut builder = Request::builder().method(&method).uri(url);
+    match builder.headers_mut() {
+        Some(h) => *h = headers,
+        None => {
+            let error = "Failed to add headers to request";
+            error!("Internal Server Error: {error}");
+            let page = error_page(StatusCode::INTERNAL_SERVER_ERROR, error);
+            return Ok(page);
+        }
     }
 
-    let request = match builder.body(body) {
+    let req = match builder.body(body) {
         Ok(r) => r,
-        Err(e) => return Ok(error_page(StatusCode::INTERNAL_SERVER_ERROR, e)),
+        Err(e) => {
+            error!("Internal Server Error: {e}");
+            return Ok(error_page(StatusCode::INTERNAL_SERVER_ERROR, e));
+        }
     };
 
-    match state.client.request(request).await {
+    match state.client.request(req).await {
         Ok(res) => {
-            info!("{} {}", format_req(&parts.method, &parts.uri), res.status());
+            info!("{} {}", format_req(&method, &uri), res.status());
             let res = res.map(Body::new);
             Ok(res)
         }
 
-        Err(err) => Ok(error_page(StatusCode::BAD_GATEWAY, err)),
+        Err(err) => {
+            error!("Bad Gateway: {err}");
+            let page = error_page(StatusCode::BAD_GATEWAY, err);
+            Ok(page)
+        }
     }
 }
